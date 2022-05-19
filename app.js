35249dc1183ce3bd2cc4b3b1a91370119dc019a7
julianparra <julianparraos147@gmail.com>
2022-05-19 09:39:12 -0500

    servidor insomnia

--- app.js
+++ app.js
@@ -10,12 +10,28 @@ app.get('/', (req, res) => {
     res.send('hola mundo')
 })
 
-app.post('/registro', (req, res) => {
+app.post('/login', (req, res) => {
     let name = req.body.name;
     let password = req.body.password;
     res.send(`Su nombre es:  ${name} y su contraseña  es: ${password}`);
-})
+});
+app.post('/registre', (req, res) => {
+    let user = req.body.user;
+    let contraseña = req.body.contraseña;
+    res.send(`su usuario es ${user} \n, su contraseña es: ${contraseña}`);
+  });
 
+app.post('/registro', (req, res) => {
+    let nombres = req.body.nombres;
+    let apellido = req.body.apellido;
+    let telefono = req.body.telefono;
+    let  gmail = req.body.gmail;
+    let fecha_nacimiento = req.body.fecha_nacimiento;
+    let genero = req.body.genero;
+    let contraseña = req.body.contraseña;
+    let confi_contraseña = req.body.confi_contraseña;
+    res.send(`su registro nombre es: ${nombres},su apellido es: ${apellido},su telefono es ${telefono}, su correo es ${gmail}, su fecha de nacimiento es ${fecha_nacimiento}, su genero es:${genero}, su contraseña es:${contraseña},su contraseña de confirmacion es: ${confi_contraseña}`);
+  })
 
   
 app.listen(port, () => {
